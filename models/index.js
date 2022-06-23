6b2b2c0f483a2c95b25a3ad2ae9468666764ecd6
Aaron Mandel <aamandel@ucsc.edu>
2022-06-23 15:04:46 -0700

    added upvote model and route

--- models/index.js
+++ models/index.js
@@ -1,12 +1,48 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
 // create associations
+// users have many posts
 User.hasMany(Post, {
     foreignKey: 'user_id'
 });
+// posts belong to one user
 Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post, };
+// users vote on many posts
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+// posts have many users who voted on them
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+// a vote belongs to a single user
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+// a vote belongs to a single post
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+// a user has many votes
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+// a post has many votes
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
